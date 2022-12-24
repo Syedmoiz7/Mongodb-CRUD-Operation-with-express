968c6d453659abd040b8634e084f7f6822365f5a
Syedmoiz7 <syedzada7866@gmail.com>
2022-12-24 10:59:32 +0500

    database ueser changed

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import cors from "cors"
 
 const app = express()
 const port = process.env.PORT || 5000
-const mongodbURI = process.env.mongodbURI || "mongodb+srv://Syedmoiz7:javascript@cluster0.3psj2vc.mongodb.net/?retryWrites=true&w=majority"
+const mongodbURI = process.env.mongodbURI || "mongodb+srv://dbmoiz:dbmoiz@cluster0.3psj2vc.mongodb.net/?retryWrites=true&w=majority"
 
 app.use(cors());
 app.use(express.json())
